5de20338f653b447e4cc52e32f38dd5bbb694929
Kalin Li <ChocolateQiaoli@outlook.com>
2020-01-07 12:50:43 +0800

    recp
    Resolve #1
    
    Signed-off-by: Kalin Li <ChocolateQiaoli@outlook.com>

---
+++ os.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo useless
+    exit 1
+fi
+
+os=`uname -s`
+
+case $os in
+Linux)
+    comment="xclip -sel clip"
+    ;;
+Darwin)
+    comment="pbcopy"
+    ;;
+*)
+    echo "not support"
+    exit 1
+    ;;
+esac
+
+count=0
+
+for fn in $@
+do
+    case $fn in
+    *.sh | *.py)
+        annotation="# $fn"
+        ;;
+    *.mysql)
+        annotation="--- $fn"
+        ;;
+    *.html)
+        annotation="<-- $fn -->"
+        ;;    
+    .*)
+        annotation="// $fn"
+        ;;
+    esac
+
+    echo $annotation
+    cat $fn
+    ((count++))
+    if [ $# -ne $# ]; then
+        echo
+    fi
+done | $comment    
+
